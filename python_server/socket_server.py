34d123f90ba6b34e38389d5664b71c391c0270f2
Phil Howard <phil@gadgetoid.com>
2017-04-26 10:13:28 +0000

    Chmod socket file

--- python_server/socket_server.py
+++ python_server/socket_server.py
@@ -171,6 +171,7 @@ def run(output_device):
     global running
 
     server = VUServer(SOCKET_FILE, VUHandler, output_device)
+    os.chmod(SOCKET_FILE,0777)
     thread_server = threading.Thread(target=server.serve_forever)
 
     def shutdown(signum, frame):
